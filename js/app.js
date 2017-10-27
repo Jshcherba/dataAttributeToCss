e636899ddb1dcc190c416f9c78a7dcf6170cd343
yscherba@speroteck.com <yscherba@speroteck.com>
2017-10-27 15:27:23 +0300

    reviews

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-    console.log('here we go!');
+    $('a.plus').on('click', function () {
+        var parent = $(this).parent().find('.reviews');
+        var attrValue = parseInt(parent.attr("data-review"));
+        if (attrValue < 100) {
+            parent.attr("data-review", attrValue + 10);
+        }
+    });
+    $('a.minus').on('click', function () {
+        var parent = $(this).parent().find('.reviews');
+        var attrValue = parseInt(parent.attr("data-review"));
+        if (attrValue > 0) {
+            parent.attr("data-review", attrValue - 10);
+        }
+    });
 });
